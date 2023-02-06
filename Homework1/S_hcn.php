f19786e8cc3d8f9c07da8daffe3371cd353831e9
Nguyen Thi Ut Vien <97380870+nguyenthiutvien@users.noreply.github.com>
2023-02-06 09:45:25 +0700

    finish homework

---
+++ Homework1/S_hcn.php
@@ -0,0 +1,85 @@
+<!-- 
+    
+ -->
+ <!DOCTYPE html>
+<html>
+<head>
+    <title>Hình chữ nhật</title>
+    <style>
+        table{
+            width: 500px;
+            margin: auto;
+            background: #ffca97;
+        }
+        td{
+            padding: 10px;
+        }
+        .center{
+            text-align: center;
+            font-weight: bold;
+            background: #ffa500;
+            color: #ffffff;
+        }
+        input{
+            width: 300px;
+        }
+        button{
+            background: #34a853;
+            border: none;
+            padding: 10px 20px;
+            font-weight: bold;
+        }
+        button:hover{
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+<?php
+    ini_set('display_errors',0);
+    $width = $_POST['width'];
+    $height = $_POST['height'];
+    // $perimeter = ($width+$height)*2;
+    $area = $width*$height;
+?>
+    <form action="" method="post">
+        <table>
+            <tr class="center">
+                <td colspan="2">
+                    DIỆN TÍCH HÌNH CHỮ NHẬT
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Chều rộng
+                </td>
+                <td>
+                    <input type="text" name="width">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Chều cao
+                </td>
+                <td>
+                    <input type="text" name="height">
+                </td>
+            </tr>
+            
+            <tr>
+                <td>
+                    Diện tích
+                </td>
+                <td>
+                    <input type="text" name="acreage" value="<?php echo $area ?>">
+                </td>
+            </tr>
+            <tr class="center">
+                <td colspan="2">
+                    <button type="submit">TÍNH</button>
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
